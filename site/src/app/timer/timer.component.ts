34fc76bf6216c10ef0ee6054c5f35ec2d1e28342
Alec Neevel <aneevel15@gmail.com>
2022-09-08 18:10:29 -0500

    Get timer updating

--- site/src/app/timer/timer.component.ts
+++ site/src/app/timer/timer.component.ts
@@ -11,30 +11,44 @@ export class TimerComponent implements OnInit {
   counterHasTime: boolean = false;
   timerValue: string = "25:00";
   defaultTimerValue: string = "25:00";
+  interval: any;
 
-  constructor() {}
+  constructor() {
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
 
   startCountdown(): void {
     this.countingDown = true;
     this.counterHasTime = true;
+    this.interval = setInterval((this.updateTimer), 1000, this.timerValue);
   }
 
   resumeCountdown(): void {
     this.countingDown = true;
+    this.interval = setInterval((this.updateTimer), 1000);
   }
 
   pauseCountdown(): void {
     this.countingDown = false;
+    clearInterval(this.interval);
   }
 
   stopCountdown(): void {
     this.countingDown = false;
     this.counterHasTime = false;
     this.timerValue = this.defaultTimerValue;
+    clearInterval(this.interval);
   }
 
-  updateTimer(): void {
+  updateTimer(timerValue: string): void {
+
+    let seconds = timerValue.slice(3);
+    console.log(`Seconds value ${seconds}`);
+
+    let minutes = timerValue.slice(0, 2);
+    console.log(`Minutes value ${minutes}`);
   }
+
 }
