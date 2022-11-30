ff4a15b0c29717c23d420886816ba3b2c384962b
Alec Neevel <aneevel15@gmail.com>
2022-11-29 19:08:59 -0600

    Add test data for tasks

--- site/src/app/tasks/tasks.component.ts
+++ site/src/app/tasks/tasks.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TasksComponent implements OnInit {
 
+  tasks = [
+    { id: 1, description: 'Do WebDev exercises', pomodoros: 2 },
+    { id: 2, description: 'Work on Game', pomodoros: 2},
+    { id: 3, description: 'Play rhythm guitar', pomodoros: 3}
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
