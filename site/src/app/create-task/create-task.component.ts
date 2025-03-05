f93abaffdb62e4d93ece80985c09d579eaec4213
aneevel <aneevel15@gmail.com>
2025-03-05 15:02:21 -0600

    add plus icon

--- site/src/app/create-task/create-task.component.ts
+++ site/src/app/create-task/create-task.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-create-task',
+  imports: [
+    FontAwesomeModule
+  ],
   templateUrl: './create-task.component.html',
   styleUrls: ['./create-task.component.css'],
   standalone: true
 })
 export class CreateTaskComponent implements OnInit {
+
+  faPlus = faPlus;
 
   constructor() { }
 
