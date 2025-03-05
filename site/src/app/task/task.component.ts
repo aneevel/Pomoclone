3ab896aba9492345411166bcdd68d1129e127f42
aneevel <aneevel15@gmail.com>
2025-03-05 14:53:32 -0600

    complete migration to angular19

--- site/src/app/task/task.component.ts
+++ site/src/app/task/task.component.ts
@@ -2,10 +2,10 @@ import { Component, Input, OnInit } from '@angular/core';
 
 import { Task } from './task';
 @Component({
-    selector: 'app-task',
-    templateUrl: './task.component.html',
-    styleUrls: ['./task.component.css'],
-    standalone: false
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.css'],
+  standalone: true
 })
 export class TaskComponent implements OnInit {
 
--- site/src/app/tasks/tasks.component.ts
+++ site/src/app/tasks/tasks.component.ts
@@ -1,8 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskComponent } from '../task/task.component';
+import { CreateTaskComponent } from '../create-task/create-task.component';
 
 import { TASKS } from '../task/task';
+import { NgFor } from '@angular/common';
+
 @Component({
   selector: 'app-tasks',
+  imports: [
+    TaskComponent,
+    CreateTaskComponent,
+    NgFor
+  ],
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css'],
   standalone: true
